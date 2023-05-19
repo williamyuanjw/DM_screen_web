1b634ab8e1225993cb5fb0609cddf84d60c1e487
yuanjinwie <990991552@qq.com>
2023-05-19 15:02:24 +0800

    chore: components文件

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,11 @@ export {};
 
 declare module '@vue/runtime-core' {
 	export interface GlobalComponents {
+		AButton: typeof import('ant-design-vue/es')['Button'];
+		AForm: typeof import('ant-design-vue/es')['Form'];
+		AFormItem: typeof import('ant-design-vue/es')['FormItem'];
+		AInput: typeof import('ant-design-vue/es')['Input'];
+		AInputPassword: typeof import('ant-design-vue/es')['InputPassword'];
 		HelloWorld: typeof import('./src/components/HelloWorld.vue')['default'];
 		IssuseAndPr: typeof import('./src/components/IssuseAndPr/index.vue')['default'];
 		RouterLink: typeof import('vue-router')['RouterLink'];
