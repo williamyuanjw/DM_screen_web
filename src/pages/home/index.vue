efa52a77048aa1e21dfa7adc9d0e5298a112e12a
yuanjinwie <990991552@qq.com>
2023-05-19 15:06:37 +0800

    Merge remote-tracking branch 'origin/feat-yuanjinwei-2023_05_04-home'

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,34 @@
-<template>home</template>
+<template>
+	<div class="home">
+		<IssueAndPr v-model:chartValue="IssueAndPrChart" />
+		<IssueAndPr v-model:chartValue="testCharts" />
+	</div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IssueAndPr from '@/components/IssuseAndPr/index.vue';
+import _ from 'lodash';
+import { shallowRef, onBeforeUnmount, onMounted } from 'vue';
+import { EChartsType } from 'echarts/core';
 
-<style lang="less"></style>
+const IssueAndPrChart = shallowRef<EChartsType>();
+const testCharts = shallowRef<EChartsType>();
+const chartResize = _.throttle(() => {
+	IssueAndPrChart.value?.resize && IssueAndPrChart.value?.resize();
+	testCharts.value?.resize && testCharts.value?.resize();
+}, 300);
+onMounted(() => {
+	window.addEventListener('resize', chartResize);
+});
+
+onBeforeUnmount(() => {
+	window.removeEventListener('resize', chartResize);
+});
+</script>
+
+<style lang="scss">
+.home {
+	width: 100%;
+	height: 100%;
+}
+</style>
